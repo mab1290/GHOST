e6a7ba33173ae09b9bc6ff0e0e8fdbcf10e27670
mab1290 <mab1290@rit.edu>
2017-04-03 19:12:32 -0400

    Added 'Has word' feature

--- ghost_main.py
+++ ghost_main.py
@@ -5,6 +5,9 @@ Michael Berezny, Pat Putnam, Sushant Kafle'''
 import csv
 import string
 
+#For removing punctuation
+punc_strip = str.maketrans('', '', string.punctuation)
+
 """Read Training Data"""
 data=[]
 with open("PS3_training_data.txt", "r") as f:
@@ -13,8 +16,11 @@ with open("PS3_training_data.txt", "r") as f:
         d={"ID":case[0], "Sentence":case[1], "Sentiment":case[2], "Event":case[3], "Genre":case[4]}
         data.append(d)
 
-#For removing punctuation
-punc_strip = str.maketrans('', '', string.punctuation)
+voc=[]
+for d in data:
+    for word in d["Sentence"].translate(punc_strip).split(" "):
+        voc.append(word)
+vocabulary= set(voc)
 
 #Takes sentence string, returns dicitonary of features
 def get_features(sentence):
@@ -23,6 +29,8 @@ def get_features(sentence):
     unique_words=set(word.lower() for word in bag_of_words)
     features["Number of Tokens"] = len(bag_of_words)
     features["Number of Types"] = len(unique_words)
-    
-
+    for word in vocabulary:
+        features['Has (%s)' % word] = (word in bag_of_words)
     return features
+
+print(get_features(data[0]["Sentence"]))
