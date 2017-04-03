835a77fa43e018a5f0f42cc51ba205aaf2aecd7a
mab1290 <mab1290@rit.edu>
2017-04-03 18:38:28 -0400

    reads training data to list of dictionaries

--- ghost_main.py
+++ ghost_main.py
@@ -1,2 +1,16 @@
 #GHOST: Genre, Happening, or Sentiment Tagger
 #Michael Berezny, Pat Putnam, Sushant Kafle
+
+"""Import"""
+import csv
+
+"""Read Training Data"""
+data=[]
+with open("PS3_training_data.txt", "r") as f:
+    reader=csv.reader(f,delimiter='\t')
+    for case in reader:
+        d={"ID":case[0], "Sentence":case[1], "Sentiment":case[2], "Event":case[3], "Genre":case[4]}
+        data.append(d)
+
+print(data[-3:])
+
