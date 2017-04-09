e0e10399e398ac51655097e5c7e51a3e8b901b49
Pat Putnam <prp2986@rit.edu>
2017-04-08 23:19:15 -0400

    Add function to process all custom features
    Additionally marks all feature functions as internal

--- ghost_features.py
+++ ghost_features.py
@@ -38,7 +38,7 @@ def _penn_to_wordnet(tag):
     return None
 
 #Checks if sentence has any positive words  
-def has_pos(s):
+def _has_pos(s):
     words = s.translate(punc_strip).split(" ")
     if not set(words).isdisjoint(positive):
         return 1
@@ -46,7 +46,7 @@ def has_pos(s):
         return 0
 
 #Checks if sentence has any negative words
-def has_neg(s):
+def _has_neg(s):
     words = s.translate(punc_strip).split(" ")
     if not set(words).isdisjoint(negative):
         return 1
@@ -54,38 +54,38 @@ def has_neg(s):
         return 0
 
 #Returns percentage of positive words    
-def percent_pos(s):
+def _percent_pos(s):
     words = s.translate(punc_strip).split(" ")
     return len([p for p in words if p in positive])/len(words)
 
 #Returns percentage of negative words
-def percent_neg(s):
+def _percent_neg(s):
     words = s.translate(punc_strip).split(" ")
     return len([n for n in words if n in negative])/len(words)
 
 #Returns percentage of neutral words
-def percent_neu(s):
+def _percent_neu(s):
     words = s.translate(punc_strip).split(" ")
     return (len(words)-len([p for p in words if p in positive])-len([n for n in words if n in negative]))/len(words)
 
 #Returns Sentence length
-def length(s):
+def _length(s):
     words = s.translate(punc_strip).split(" ")
     return len(words)
 
 #Returns the number of "!" in a sentence
-def count_exc(s):
+def _count_exc(s):
     return len([c for c in s if c=="!"])
 
 #Checks if sentence has '$'
-def has_dollar(s):
+def _has_dollar(s):
     if "$" in s:
         return 1
     else:
         return 0
 
 #Checks if sentence has "money"
-def has_money(s):
+def _has_money(s):
     if "money" in s:
         return 1
     else:
@@ -93,7 +93,7 @@ def has_money(s):
 
 #Returns number of Named Entities
 #http://stackoverflow.com/questions/31836058/nltk-named-entity-recognition-to-a-python-list
-def name_entities(s):
+def _name_entities(s):
      chunked = ne_chunk(pos_tag(word_tokenize(s)))
      continuous_chunk = []
      current_chunk = []
@@ -110,7 +110,7 @@ def name_entities(s):
      return len(continuous_chunk)
 
 #Checks whether sentence contains a location
-def has_location(s):
+def _has_location(s):
     loc = [blah for blah in st.tag(s.split()) if blah[1]=='LOCATION']
     if len(loc)>0:
         return 1
@@ -119,7 +119,7 @@ def has_location(s):
 
 
 #Checks whether sentece contains a quotatons
-def has_quote(s):
+def _has_quote(s):
     comp=re.compile(r'".+"')
     if comp.search(s):
         return 1
@@ -127,20 +127,20 @@ def has_quote(s):
         return 0
 
 #Returns number of characters
-def chars(s):
+def _chars(s):
     return len(s)
 
 #Returns the percentagenumber of capital words
-def cap_words(s):
+def _cap_words(s):
     words = s.translate(punc_strip).split(" ")
     return len([word for word in words if word.isupper()])/len(words)
 
 #Returns the percentage of capital letters
-def cap_chars(s):
+def _cap_chars(s):
     return len([char for char in s if char.isupper()])/len(s)
 
 #Checks for the "go" lemma, should recognize 'going', 'went', etc.
-def has_go(s):
+def _has_go(s):
     lemmas=[]
     for wt in TB(s).tags:
         lemmas.append(Word(wt[0]).lemmatize(_penn_to_wordnet(wt[1])))
@@ -151,7 +151,7 @@ def has_go(s):
     
 #Checks whether sentece contains more than two adjacent repeated letters
 #Doesn't really occur in English, could indicate exaggerated emotions e.g. "booooooring"
-def has_repeats(s):
+def _has_repeats(s):
     comp=re.compile(r"(\w)\1{2,}")
     if comp.search(s):
         return 1
@@ -159,33 +159,64 @@ def has_repeats(s):
         return 0
 
 #Returns the number of nouns in a sentences    
-def count_nouns(s):
+def _count_nouns(s):
     return len([w for w in TB(s).tags if w[1]=="NN" or "NNS"])
 
 #Returns the number of proper nouns in a sentences    
-def count_Pnouns(s):
+def _count_Pnouns(s):
     return len([w for w in TB(s).tags if w[1]=="NNP" or "NNPS"])
 
 #Returns the number of adjectives in a sentences    
-def count_adj(s):
+def _count_adj(s):
     return len([w for w in TB(s).tags if w[1]=="JJ" or "JJR"])
 
 #Returns the number of superlatives in a sentences    
-def count_super(s):
+def _count_super(s):
     return len([w for w in TB(s).tags if w[1]=="JJS"])
 
 
 #Returns the number of verbs in a sentences    
-def count_verb(s):
+def _count_verb(s):
     return len([w for w in TB(s).tags if w[1]=="VB" or "VBZ" or "VBP" or "VBD" or "VBN" or "VBG"])
 
 #Returns the number of personal pronouns in a sentences    
-def count_pro(s):
+def _count_pro(s):
     return len([w for w in TB(s).tags if w[1]=="PRP"])
 
 #Returns the TextBlob sentiment polarity between -1 and 1 where -1 is the msot negative
-def blob_sent(s):
+def _blob_sent(s):
     return TB(s).sentiment.polarity
+
+
+def process_features(string):
+    features = {}
+
+    features['has_pos_word'] = _has_pos(string)
+    features['has_neg_word'] = _has_neg(string)
+    features['percent_pos_words'] = _percent_pos(string)
+    features['percent_neg_words'] = _percent_neg(string)
+    features['percent_neu_words'] = _percent_neu(string)
+    features['length'] = _length(string)
+    features['count_exclamation_mark'] = _count_exc(string)
+    features['has_dollar_sign'] = _has_dollar(string)
+    features['contains_word_money'] = _has_money(string)
+    features['count_of_named_entities'] = _name_entities(string)
+    features['has_location'] = _has_location(string)
+    features['has_quotations'] = _has_quote(string)
+    features['count_of_characters'] = _chars(string)
+    features['percent_upper_case_words'] = _cap_words(string)
+    features['percent_upper_case_chars'] = _cap_chars(string)
+    features['has_go_lemma'] = _has_go(string)
+    features['has_repeated_letters'] = _has_repeats(string)
+    features['count_of_nouns'] = _count_nouns(string)
+    features['count_of_proper_nouns'] = _count_Pnouns(string)
+    features['count_of_adjectives'] = _count_adj(string)
+    features['count_of_superlatives'] = _count_super(string)
+    features['count_of_verbs'] = _count_verb(string)
+    features['count_of_pronouns'] = _count_pro(string)
+    features['textblob_sentiment_polarity'] = _blob_sent(string)
+
+    return features
 
 '''
 #This is just here so I can test functions
