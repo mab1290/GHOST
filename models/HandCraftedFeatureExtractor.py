848821ab6ba9478bdb8e3553d509f1b154abb6d8
Pat Putnam <prp2986@rit.edu>
2017-04-10 13:32:38 -0400

    Return vectors from HandCraftedFeatureExctractor

--- models/HandCraftedFeatureExtractor.py
+++ models/HandCraftedFeatureExtractor.py
@@ -1,19 +1,23 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction import DictVectorizer
 from ghost_features import *
 
 class HandCraftedFeatureExtractor(BaseEstimator, TransformerMixin):
 
     def __init__(self):
         pass
+        self.vect = DictVectorizer()
 
     def get_features(self, text):
         """Helper code to compute average word length of a name"""
         features = process_features(text)
-        return list(features.values())
+        return features
 
     def transform(self, texts, y=None):
         """The workhorse of this feature extractor"""
-        return list(map(self.get_features, texts))
+        dicts = list(map(self.get_features, texts))
+        return self.vect.fit_transform(dicts).toarray()
+
 
     def fit(self, df, y=None):
         """Returns `self` unless something different happens in train and test"""
